7f242f2cf9dd9bc0d3929a1ce1bef247254de5e6
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-21 15:39:44 +0100

    25. Added security Authorization

--- spring-security/src/main/java/org/anurag/spring_security/controller/WelcomeController.java
+++ spring-security/src/main/java/org/anurag/spring_security/controller/WelcomeController.java
@@ -7,8 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome() {
+        return "Hello all!";
+    }
+
+    @GetMapping("/user")
+    public String welcomeUser() {
         return "Hello user!";
+    }
+
+    @GetMapping("/admin")
+    public String welcomeAdmin() {
+        return "Hello Admin!";
     }
 }
