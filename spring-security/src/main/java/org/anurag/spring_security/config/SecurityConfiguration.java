11f21a2ef4c9799a0c55fa2627715f730a7bfed0
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-21 16:45:52 +0100

    26. JDBC Authentication with H2
    With default schema

--- spring-security/src/main/java/org/anurag/spring_security/config/SecurityConfiguration.java
+++ spring-security/src/main/java/org/anurag/spring_security/config/SecurityConfiguration.java
@@ -1,16 +1,44 @@
 package org.anurag.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+    /* Since we are using in memory database
+    *  Spring will configure this data source for us */
+
+    // Authentication
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("user")
+                                .password("pass1234")
+                                .roles("USER")
+                )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("admin")
+                                .roles("ADMIN")
+                );
+    }
+
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         // set your configuration here
@@ -23,12 +51,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .password("test")
                 .roles("ADMIN");
     }
+    */
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    // Authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
