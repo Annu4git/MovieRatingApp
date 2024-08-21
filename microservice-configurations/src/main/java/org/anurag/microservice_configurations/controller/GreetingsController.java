07eb19366832ab6a8d56b092d3e0f2945d08a373
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-21 11:32:58 +0100

    19. Refresh functionality added

--- microservice-configurations/src/main/java/org/anurag/microservice_configurations/controller/GreetingsController.java
+++ microservice-configurations/src/main/java/org/anurag/microservice_configurations/controller/GreetingsController.java
@@ -3,6 +3,7 @@ package org.anurag.microservice_configurations.controller;
 import org.anurag.microservice_configurations.config.DBSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/greetings")
+@RefreshScope
 public class GreetingsController {
 
     @Value("${greetings.message}")
