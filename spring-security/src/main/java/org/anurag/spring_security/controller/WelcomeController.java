e179e070af925ecbb536eb23eefa2d234a34cdb9
Anurag Chaturvedi <anuragcodejam@gmail.com>
2024-08-21 11:56:50 +0100

    20. Spring security module added

---
+++ spring-security/src/main/java/org/anurag/spring_security/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package org.anurag.spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Hello user!";
+    }
+}
